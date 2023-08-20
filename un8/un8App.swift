dc415bb9f2687ddab4b92078e23d913ea3ef1b37
Fiona <fifiisfiona.xichen291226@gmail.com>
2023-08-20 22:01:46 +0800

    Initial Commit

---
+++ un8/un8App.swift
@@ -0,0 +1,17 @@
+//
+//  un8App.swift
+//  un8
+//
+//  Created by Xichen Dong on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct un8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
